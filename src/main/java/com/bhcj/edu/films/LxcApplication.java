9f584584ba2fc152eb71c4886e521ae5759e553c
lixiangchao <lixiangchao@iqiju.com>
2021-04-06 16:13:58 +0800

    hello films

---
+++ src/main/java/com/bhcj/edu/films/LxcApplication.java
@@ -0,0 +1,13 @@
+package com.bhcj.edu.films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhcj/edu/films/LxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhcj.edu.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
